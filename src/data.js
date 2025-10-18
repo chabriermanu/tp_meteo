7d1eb4d6f92f289f42e828d0f1617d22fb1da35d
chabriermanu <chabrier.manu@gmail.com>
2025-10-18 19:10:55 +0200

    tp_fini

---
+++ src/data.js
@@ -0,0 +1,168 @@
+export const cdaData = [
+
+    {
+        "name": "DUNKERQUE",
+        "category": 'CDA',
+        "lat": 51.034606791697996,
+        "lon": 2.376409003224757
+    },
+
+    {
+        "name": "AMIENS",
+        "category": 'CDA',
+        "lat": 49.89270983479487,
+        "lon": 2.2987550266639163
+    },
+
+    {
+        "name": "LES GREVES LANGUEUX",
+        "category": 'CDA',
+        "lat": 48.49556165447708,
+        "lon": -2.694826113394654
+    },
+
+    {
+        "name": "BEAUMONT",
+        "category": 'CDA',
+        "lat": 45.75420475327472,
+        "lon": 3.086003054037277
+    }];
+export const dwwmData = [
+    {
+        "name": "LE HAVRE",
+        "category": 'DWWM',
+        "lat": 49.49570214077265,
+        "lon": 0.10183943325441415
+    },
+    {
+        "name": "Rouen Saint-Etienne-du-Rouvray",
+        "category": 'DWWM',
+        "lat": 49.39137191151561,
+        "lon": 1.0728841975598338
+    },
+
+    {
+        "name": "FONTENAY LE COMTE",
+        "category": 'DWWM',
+        "lat": 46.45970389722367,
+        "lon": -0.7959257408343696
+    },
+
+    {
+        "name": "OLIVET",
+        "category": 'DWWM',
+        "lat": 47.8760831751405,
+        "lon": 1.9096489070355431
+    },
+    {
+        "name": "Centre d'Angoulême / Mornac",
+        "category": 'DWWM',
+        "lat": 45.70218327164594,
+        "lon": 0.3028674257166668
+    },
+
+    {
+        "name": "NIORT",
+        "category": 'DWWM',
+        "lat": 46.31227094653273,
+        "lon": -0.4396807826805044
+    },
+    {
+        "name": "Centre de Bayonne",
+        "category": 'DWWM',
+        "lat": 43.50535215122969,
+        "lon": -1.4708551473866587
+    },
+    {
+        "name": "TARBES",
+        "category": 'DWWM',
+        "lat": 43.25429472367773,
+        "lon": 0.07357302747246905
+    },
+    {
+        "name": "Lourdes",
+        "category": 'DWWM',
+        "lat": 43.09141372142772,
+        "lon": -0.04570173672170328
+    },
+    {
+        "name": "Auch",
+        "category": 'DWWM',
+        "lat": 43.64605980423518,
+        "lon": 0.5905625937808718
+    },
+    {
+        "name": "Albi",
+        "category": 'DWWM',
+        "lat": 43.93444184775962,
+        "lon": 2.1837507343642124
+    },
+
+    {
+        "name": "NICE",
+        "category": 'DWWM',
+        "lat": 43.72817781937217,
+        "lon": 7.286002611097077
+    },
+    {
+        "name": "Centre de Marseille St Jérôme",
+        "category": 'DWWM',
+        "lat": 43.32858120252916,
+        "lon": 5.414976286999876
+    }
+];
+export const bothData = [
+    {
+        "name": "POMPEY",
+        "category": 'CDA et DWWM',
+        "lat": 48.77663907676138,
+        "lon": 6.139756954680775
+    },
+    {
+        "name": "BREST",
+        "category": 'CDA et DWWM',
+        "lat": 48.41342690346579,
+        "lon": -4.500777616498668
+    },
+    {
+        "name": "CHATELLERAULT",
+        "category": 'CDA et DWWM',
+        "lat": 46.81315814870116,
+        "lon": 0.5330065773521216
+    },
+    {
+        "name": "BRIVE LA GAILLARDE",
+        "category": 'CDA et DWWM',
+        "lat": 45.14842293897074,
+        "lon": 1.5547959079729634
+    },
+    {
+        "name": "BEGLES",
+        "category": 'CDA et DWWM',
+        "lat": 44.80201467203979,
+        "lon": -0.556241511614755
+    },
+
+    {
+        "name": "MONT DE MARSAN",
+        "category": 'CDA et DWWM',
+        "lat": 43.89760730440761,
+        "lon": -0.4913627111256291
+    },
+
+    {
+        "name": "ST JEAN DE VEDAS",
+        "category": 'CDA et DWWM',
+        "lat": 43.56495888754544,
+        "lon": 3.8470319454840944
+    },
+
+    {
+        "name": "ISTRES",
+        "category": 'CDA et DWWM',
+        "lat": 43.4888978841649,
+        "lon": 4.988998490710979
+    }
+];
+
+
--- src/main.js
+++ src/main.js
@@ -1,171 +1,120 @@
-// Data retrieved from https://www.vikjavev.no/ver/#2020-04-15,2020-04-16
+(async () => {
+    const topology = await fetch(
+        'https://code.highcharts.com/mapdata/countries/fr/fr-all.topo.json'
+    ).then(response => response.json());
 
-Highcharts.chart('container', {
-    chart: {
-        type: 'spline',
-        scrollablePlotArea: {
-            minWidth: 600,
-            scrollPositionX: 1
-        }
-    },
-    title: {
-        text: 'Wind speed during a day',
-        align: 'left'
-    },
-    subtitle: {
-        text: '29th of February, 2024 at two locations in Vik i Sogn, Norway',
-        align: 'left'
-    },
-    xAxis: {
-        type: 'datetime',
-        labels: {
-            overflow: 'justify'
-        }
-    },
-    yAxis: {
-        title: {
-            text: 'Wind speed (m/s)'
+    Highcharts.mapChart('container', {
+        chart: {
+            map: topology
         },
-        minorGridLineWidth: 0,
-        gridLineWidth: 0,
-        alternateGridColor: null,
-        plotBands: [{ // Light air
-            from: 0.3,
-            to: 1.5,
-            color: 'rgba(68, 170, 213, 0.1)',
-            label: {
-                text: 'Light air',
-                style: {
-                    opacity: 0.7
-                }
-            }
-        }, { // Light breeze
-            from: 1.5,
-            to: 3.3,
-            color: 'rgba(0, 0, 0, 0)',
-            label: {
-                text: 'Light breeze',
-                style: {
-                    opacity: 0.7
-                }
-            }
-        }, { // Gentle breeze
-            from: 3.3,
-            to: 5.5,
-            color: 'rgba(68, 170, 213, 0.1)',
-            label: {
-                text: 'Gentle breeze',
-                style: {
-                    opacity: 0.7
-                }
-            }
-        }, { // Moderate breeze
-            from: 5.5,
-            to: 8,
-            color: 'rgba(0, 0, 0, 0)',
-            label: {
-                text: 'Moderate breeze',
-                style: {
-                    opacity: 0.7
-                }
-            }
-        }, { // Fresh breeze
-            from: 8,
-            to: 11,
-            color: 'rgba(68, 170, 213, 0.1)',
-            label: {
-                text: 'Fresh breeze',
-                style: {
-                    opacity: 0.7
-                }
-            }
-        }, { // Strong breeze
-            from: 11,
-            to: 14,
-            color: 'rgba(0, 0, 0, 0)',
-            label: {
-                text: 'Strong breeze',
-                style: {
-                    opacity: 0.7
-                }
-            }
-        }, { // Near Gale
-            from: 14,
-            to: 17,
-            color: 'rgba(68, 170, 213, 0.1)',
-            label: {
-                text: 'Near gale',
-                style: {
-                    opacity: 0.7
-                }
-            }
-        }, { // Fresh Gale
-            from: 17,
-            to: 20.5,
-            color: 'rgba(0, 0, 0, 0)',
-            label: {
-                text: 'Fresh gale',
-                style: {
-                    opacity: 0.7
-                }
-            }
-        }, { // Strong Gale
-            from: 20.5,
-            to: 24,
-            color: 'rgba(68, 170, 213, 0.1)',
-            label: {
-                text: 'Strong gale',
-                style: {
-                    opacity: 0.7
-                }
-            }
-        }]
-    },
-    tooltip: {
-      formatter: function(){
-        const icom = this.y >10 ? "" :"";
-        return `<b>${this.service.name}</b><br/>
-                température : ${this.y})c ${icon}`;
-    },
-        valueSuffix: ')c'
-    },
-    plotOptions: {
-        spline: {
-            lineWidth: 4,
-            states: {
-                hover: {
-                    lineWidth: 5
-                }
+        title: {
+            text: 'Les centres AFPA proposant les formations CDA et DWWM.',
+            align: 'left'
+        },
+        legend: {
+            enabled: true,
+            itemStyle: {
+                fontSize: '16px',
+                fontWeight: 'bold'
             },
-            marker: {
-                enabled: false
+            itemDistance: 25
+        },
+        mapNavigation: {
+            enabled: true
+        },
+        tooltip: {
+            headerFormat: '',
+            pointFormat: '<b>{point.name}</b><br>Lat: {point.lat:.2f}, Lon: {point.lon:.2f}'
+        },
+        plotOptions: {
+            mappoint: {
+                cluster: {
+                    enabled: true,
+                    allowOverlap: false,
+                    animation: {
+                        duration: 450
+                    },
+                    layoutAlgorithm: {
+                        type: 'grid',
+                        gridSize: 70
+                    },
+                    zones: [
+                        { from: 1, to: 4, marker: { radius: 13 } },
+                        { from: 5, to: 9, marker: { radius: 15 } },
+                        { from: 10, to: 15, marker: { radius: 17 } },
+                        { from: 16, to: 20, marker: { radius: 19 } },
+                        { from: 21, to: 100, marker: { radius: 21 } }
+                    ]
+                }
+            }
+        },
+        series: [
+            {
+                borderColor: '#A0A0A0',
+                nullColor: 'rgba(177, 244, 177, 0.5)',
+                showInLegend: false
             },
-            pointInterval: 3600000, // one hour
-            pointStart: '2014-02-29'
-        }
-    },
-    series: [{
-        name: 'Hestavollane',
-        data: [
-            12.9, 13.8, 10.2, 8.4, 10.0, 9.2, 10.0,
-            12.2, 13.2, 12.7, 12.5, 11.4, 10.4,
-            7.9, 8.0, 11.4, 11.5, 12.0, 12.0,
-            10.4, 11.2, 11.5, 12.2, 11.5, 8.3
+            {
+                type: 'mappoint',
+                name: 'Centres CDA',
+                color: '#28a745',
+                showInLegend: true,
+                colorKey: 'clusterPointsAmount',
+                dataLabels: {
+                    verticalAlign: 'top'
+                },
+                data: [
+                    { name: "DUNKERQUE", lat: 51.0346, lon: 2.3764 },
+                    { name: "AMIENS", lat: 49.8927, lon: 2.2988 },
+                    { name: "LES GREVES LANGUEUX", lat: 48.4956, lon: -2.6948 },
+                    { name: "BEAUMONT", lat: 45.7542, lon: 3.0860 }
+                ]
+            },
+            {
+                type: 'mappoint',
+                name: 'Centres DWWM',
+                color: '#0077be',
+                showInLegend: true,
+                colorKey: 'clusterPointsAmount',
+                dataLabels: {
+                    verticalAlign: 'top'
+                },
+                data: [
+                    { name: "LE HAVRE", lat: 49.4957, lon: 0.1018 },
+                    { name: "Rouen Saint-Etienne-du-Rouvray", lat: 49.3914, lon: 1.0729 },
+                    { name: "FONTENAY LE COMTE", lat: 46.4597, lon: -0.7959 },
+                    { name: "OLIVET", lat: 47.8761, lon: 1.9096 },
+                    { name: "Centre d'Angoulême / Mornac", lat: 45.7022, lon: 0.3029 },
+                    { name: "NIORT", lat: 46.3123, lon: -0.4397 },
+                    { name: "Centre de Bayonne", lat: 43.5054, lon: -1.4709 },
+                    { name: "TARBES", lat: 43.2543, lon: 0.0736 },
+                    { name: "Lourdes", lat: 43.0914, lon: -0.0457 },
+                    { name: "Auch", lat: 43.6461, lon: 0.5906 },
+                    { name: "Albi", lat: 43.9344, lon: 2.1838 },
+                    { name: "NICE", lat: 43.7282, lon: 7.2860 },
+                    { name: "Centre de Marseille St Jérôme", lat: 43.3286, lon: 5.4150 }
+                ]
+            },
+            {
+                type: 'mappoint',
+                name: 'Centres CDA et DWWM',
+                color: '#f90505',
+                showInLegend: true,
+                colorKey: 'clusterPointsAmount',
+                dataLabels: {
+                    verticalAlign: 'top'
+                },
+                data: [
+                    { name: "POMPEY", lat: 48.7766, lon: 6.1398 },
+                    { name: "BREST", lat: 48.4134, lon: -4.5008 },
+                    { name: "CHATELLERAULT", lat: 46.8132, lon: 0.5330 },
+                    { name: "BRIVE LA GAILLARDE", lat: 45.1484, lon: 1.5548 },
+                    { name: "BEGLES", lat: 44.8020, lon: -0.5562 },
+                    { name: "MONT DE MARSAN", lat: 43.8976, lon: -0.4914 },
+                    { name: "ST JEAN DE VEDAS", lat: 43.5650, lon: 3.8470 },
+                    { name: "ISTRES", lat: 43.4889, lon: 4.9890 }
+                ]
+            }
         ]
-
-    }, {
-        name: 'Vik',
-        data: [
-            null, 1.3, 1.1, 0.8, 1.8, 1.7, 0.8,
-            0.8, 1.0, 1.0, 1.0, 0.8, 1.4,
-            1.3, 2.9, 6.1, 6.4, 6.6, 6.4,
-            6.3, 5.4, 3.9, 3.0, 1.7, 1.4
-        ]
-    }],
-    navigation: {
-        menuItemStyle: {
-            fontSize: '10px'
-        }
-    }
-});
-
+    });
+})();
